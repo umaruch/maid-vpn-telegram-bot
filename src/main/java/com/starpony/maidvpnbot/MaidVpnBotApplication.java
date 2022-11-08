5b2086b60a8b60517da75b0d3b429046b79d67c5
umaruch <60070943+umaruch@users.noreply.github.com>
2022-11-08 23:05:32 +0500

    init app

---
+++ src/main/java/com/starpony/maidvpnbot/MaidVpnBotApplication.java
@@ -0,0 +1,13 @@
+package com.starpony.maidvpnbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidVpnBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaidVpnBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starpony/maidvpnbot/MaidVpnBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starpony.maidvpnbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidVpnBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
